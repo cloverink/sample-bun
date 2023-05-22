eda745f9cefae747db4a4b7f8f1dfd5b94718187
Aran Chananar <cloverink@gmail.com>
2023-05-23 00:26:21 +0700

    feat: hello world

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    const body = figlet.textSync('Aran Cloverink Chananar');
+    return new Response(body);
+  },
+});
+
+
+console.log(`Listening on http://localhost:${server.port}...`);
